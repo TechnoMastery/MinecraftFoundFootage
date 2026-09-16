c4ad507a6878f595cb127d73d1390a5a89123486
minheur2000 <adrien.rousselet@hotmail.com>
2026-09-16 20:37:14 +0200

    refactor(setup): Level0IntercomBasic

--- src/main/java/net/minheur/world/events/level0/Level0IntercomBasic.java
+++ src/main/java/net/minheur/world/events/level0/Level0IntercomBasic.java
@@ -14,37 +14,28 @@ public class Level0IntercomBasic extends AbstractEvent {
 
     @Override
     public void init(World world) {
-        if (!(BackroomsLevels.getLevel(world).orElse(BackroomsLevels.OVERWORLD_REPRESENTING_BACKROOMS_LEVEL) instanceof Level0BackroomsLevel level)) {
+        if (!(BackroomsLevels.getLevel(world).orElse(BackroomsLevels.OVERWORLD_REPRESENTING_BACKROOMS_LEVEL) instanceof Level0BackroomsLevel level))
             return;
-        }
 
         int intercomCount = level.getIntercomCount();
         Random random = Random.create();
 
         if (intercomCount <= 1) {
             int rand = random.nextBetween(1, 2);
-            if (rand == 1) {
-                playSoundWithRandLocation(world, ModSounds.INTERCOM_BASIC1, 25, 20);
-            } else {
-                playSoundWithRandLocation(world, ModSounds.INTERCOM_BASIC2, 25, 20);
-            }
+
+            if (rand == 1) playSoundWithRandLocation(world, ModSounds.INTERCOM_BASIC1, 25, 20);
+            else playSoundWithRandLocation(world, ModSounds.INTERCOM_BASIC2, 25, 20);
         }
-
-
-        else if (intercomCount == 2) {
-            playSoundWithRandLocation(world, ModSounds.INTERCOM_REVERSED, 25, 20);
-        }
-
+        else if (intercomCount == 2) playSoundWithRandLocation(world, ModSounds.INTERCOM_REVERSED, 25, 20);
 
         else {
-            int rand = random.nextBetween(1, 1);
+            int rand = random.nextBetween(1, 2);
             if (rand == 1) {
                 playSoundWithRandLocation(world, ModSounds.INTERCOM_FRIEND, 25, 20);
                 friend = true;
                 duration = 800;
-            } else {
-                playSoundWithRandLocation(world, ModSounds.INTERCOM_REVERSED, 25, 20);
             }
+            else playSoundWithRandLocation(world, ModSounds.INTERCOM_REVERSED, 25, 20);
         }
 
         level.addIntercomCount();
@@ -52,19 +43,16 @@ public class Level0IntercomBasic extends AbstractEvent {
 
     @Override
     public void ticks(int ticks, World world) {
-        if (!(BackroomsLevels.getLevel(world).orElse(BackroomsLevels.OVERWORLD_REPRESENTING_BACKROOMS_LEVEL) instanceof Level0BackroomsLevel level)) {
+        if (!(BackroomsLevels.getLevel(world).orElse(BackroomsLevels.OVERWORLD_REPRESENTING_BACKROOMS_LEVEL) instanceof Level0BackroomsLevel level))
             return;
-        }
 
-        if (friend){
-            if (ticks == 460){
-                level.setLightState(BackroomsLevelWithLights.LightState.FLICKER);
-            } else if (ticks == 528){
+        if (friend) {
+            if (ticks == 460) level.setLightState(BackroomsLevelWithLights.LightState.FLICKER);
+            else if (ticks == 528) {
                 level.setLightState(BackroomsLevelWithLights.LightState.OFF);
                 playSound(world, ModSounds.LIGHTS_OUT);
-            } else if (ticks == 656){
-                level.setLightState(BackroomsLevelWithLights.LightState.ON);
             }
+            else if (ticks == 656) level.setLightState(BackroomsLevelWithLights.LightState.ON);
         }
     }
 
